baac0f32eecde179696a088370e526c9ef4cf479
Galahad0406 <63480352+Galahad0406@users.noreply.github.com>
2026-02-23 14:31:59 -0800

    Update and rename MarketAnalysisReport.tsx to Map.tsx

---
+++ components/Map.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import L from "leaflet"
+
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png"
+})
+
+export default function Map({ lat, lng }: { lat: number; lng: number }) {
+  return (
+    <MapContainer
+      center={[lat, lng]}
+      zoom={13}
+      style={{ height: "400px", width: "100%" }}
+    >
+      <TileLayer
+        attribution="&copy; OpenStreetMap contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[lat, lng]}>
+        <Popup>Selected Location</Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
